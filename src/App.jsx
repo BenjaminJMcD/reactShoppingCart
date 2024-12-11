98bcfa2bc9764765d00c2a70af88a4aeb2c3ea1f
BenjaminJMcD <BenjaminJMcD@outlook.com>
2024-12-11 15:50:59 -0600

    build: abandoned fetchItems.jsx. added useEffect to App with conditional fetch based on filter state

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,73 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Cards from './Cards';
-import fetchItems from './fetchItems';
+
 import shoppingCart from './assets/grocery-store.png';
 
 function App() {
 
-  const [itemList, setItemList] = useState([]);
+  const [items, setItems] = useState([]);
+  const [filter, setFilter] = useState(null);
 
-  
-  const items = fetchItems();
+  useEffect(() => {
+    const fetchData = async () => {
+      if (filter == null) {
+        const response = await fetch('https://fakestoreapi.com/products')
+        const list = await response.json();
+        setItems(list);
+      }
+      else if (filter == 'electronics') {
+        const response = await fetch('https://fakestoreapi.com/products/category/electronics')
+        const list = await response.json();
+        setItems(list);
+      }
+      else if (filter == `women's apparel`) {
+        const response = await fetch(`https://fakestoreapi.com/products/category/women's%20clothing`)
+        const list = await response.json();
+        setItems(list);
+      }
+      else if (filter == `men's apparel`) {
+        const response = await fetch(`https://fakestoreapi.com/products/category/men's%20clothing`)
+        const list = await response.json();
+        setItems(list);
+      }
+      else if (filter == `jewelry`) {
+        const response = await fetch('https://fakestoreapi.com/products/category/jewelery')
+        const list = await response.json();
+        setItems(list);
+      }
+    }
+    fetchData();
+  }, [filter]);
 
-  console.log(items);
+  function handleFilter(e) {
+    if (e.target.innerText == "FANCY SHOPS") {
+      setFilter(null);
+    }
+    else {
+      setFilter(e.target.innerText.toLowerCase());
+    }
+  }
 
-
+  console.log(filter);
 
   return (
     <div className="app">
       <div className="header">
-        <h1 className="title">FANCY SHOPS</h1>
+        <h1 onClick={handleFilter} className="title">FANCY SHOPS</h1>
       </div>
       <div className="navbar">
         <nav>
-          <h3>Electronics</h3>
-          <h3>Men's Apparel</h3>
-          <h3>Women's Apparel</h3>
-          <h3>Cart Placeholder</h3>
+          <p onClick={handleFilter}>Electronics</p>
+          <p onClick={handleFilter}>Men's Apparel</p>
+          <p onClick={handleFilter}>Women's Apparel</p>
+          <p onClick={handleFilter}>Jewelry</p>
         </nav>
         <div className="shopIcon-Dynamic"><img src={shoppingCart} alt="Shopping Cart Image" className="shoppingCartImg" /></div>
       </div>
       <div className="cardsContainer">
-
           {items.map((item) => (
             <Cards
+              key = {item.id}
               image = {item.image}
               title = {item.title}
               price = {item.price}
--- src/fetchItems.jsx
+++ src/fetchItems.jsx
@@ -1,23 +1,47 @@
 import {useState, useEffect} from 'react';
 
 
+//function fetchItems() {
+
+//     const [filteredItems, setFilteredItems] = useState([]);
+
+//     useEffect(() => {
+//       fetch("https://fakestoreapi.com/products")
+//         .then((response) => {
+//           if (response.status >= 400) {
+//             throw new Error("server error");
+//           }
+//           return response.json();
+//         })
+//         .then((response) => setFilteredItems(response));
+//       }, []);
+        
+        
+        
+//     return console.log(filteredItems);
+    
+
+// }
+
+
+
+
+//}
+
 function fetchItems() {
 
-    const [filteredItems, setFilteredItems] = useState([]);
+  const [filteredItems, setFilteredItems] = useState([]);
 
-    useEffect(() => {
-        fetch("https://fakestoreapi.com/products")
-          .then((response) => {
-            if (response.status >= 400) {
-              throw new Error("server error");
-            }
-            return response.json();
-          })
-          .then((response) => setFilteredItems(response));
-    }, []);
-        
-    
-    return filteredItems;
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch('https://fakestoreapi.com/products/category/jewelery')
+      const list = await response.json();
+      setItems(list);
+    }
+    fetchData();
+  }, []);
+
+  return filteredItems
 
 }
 
