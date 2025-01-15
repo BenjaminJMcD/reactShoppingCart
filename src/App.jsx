528ea069360278a28d9339e918baeaf9666c54cd
BenjaminJMcD <BenjaminJMcD@outlook.com>
2025-01-15 13:37:56 -0600

    style: navbar converted to tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -148,7 +148,7 @@ function App() {
       <BrowserRouter>
         <Link className="no-underline text-black" to="/">
           <div onClick={resetFilter} className="bg-green-400 h-32 pt-9">
-            <h1 className="text-7xl text-center my-0">FANCY SHOPS</h1>
+            <h1 className="text-7xl text-center my-0 pl-4">FANCY SHOPS</h1>
           </div>
         </Link>
         <NavBar
--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -5,7 +5,7 @@ function NavBar({ handleFilter, total }) {
 
     return (
         <div className="w-screen inline-flex my-0 border-b pb-5">
-            <nav className="flex justify-between w-screen mx-44 pt-4 "> 
+            <nav className="flex justify-between max-w-xl pt-4 m-auto gap-10"> 
                 <Link className="no-underline text-black pt-1" to="/">
                     <p onClick={handleFilter}>Electronics</p>
                 </Link>
@@ -23,7 +23,7 @@ function NavBar({ handleFilter, total }) {
               <Link to="/cart">
                   <img src={shoppingCartIcon} alt="Shopping Cart Image" className="w-10 h-10 absolute right-12 mt-3" />
                   {total >0 &&
-                  <p className="w-6 h-6 font-bold absolute text-center pb-2 right-10 top-32 mt-2 bg-white border-4 border-black border-solid rounded-xl">{total}</p>
+                  <p className="w-7 h-7 font-bold absolute text-center pb-2 right-9 top-30 mt-1 bg-white border-4 border-black border-solid rounded-2xl">{total}</p>
                   }
               </Link>
             </div>
@@ -31,4 +31,4 @@ function NavBar({ handleFilter, total }) {
     )
 }
 
-export default NavBar;
+export default NavBar
