0435b4568df8deef1b3425bc8a0cef2366e0e33e
BenjaminJMcD <BenjaminJMcD@outlook.com>
2025-02-02 12:53:22 -0600

    style: loading image added to singleitem

--- src/SingleItem.jsx
+++ src/SingleItem.jsx
@@ -1,6 +1,7 @@
-import { useParams, Link } from "react-router-dom";
+import { useParams, Route } from "react-router-dom";
 import { useState, useEffect, useLayoutEffect } from 'react';
 import { Rating } from "@mui/material";
+import Loading from "./Loading";
 
 
 function SingleItem({ handleAddMultiple, formatPrice }) {
@@ -46,36 +47,36 @@ function SingleItem({ handleAddMultiple, formatPrice }) {
     setTimeout(dynamicHeight, 100);
 
     return (
-        <div className="grid grid-cols-[1fr_0.5fr_0.5fr] grid-rows-[20px_350px_100px] mx-20 mt-[210px] max-w-[830px] mx-auto">
+        <div>
             {loading ? (
-                <h1 className="col-span-3 text-center">LOADING</h1>
-            ) : <>
-                    <Link className="col-span-3" to="/">Back</Link>
-                    <img className="col-span-1 row-span-4 h-[400px] justify-self-center pt-3" src={product.image} alt={product.title} />
-                    <div className="row-span-1 col-span-2 place-self-center pb-5">
-                        <h1 className="mx-6 pt-10 pb-2 font-extrabold">{product.title}</h1>
-                        <p className=" mx-6 dynamic-text">{product.description}</p>
-                        <div className="flex col-span-2 justify-center pt-4 mr-2">
-                            <Rating name='half-rating-read' defaultValue={product.rating.rate} precision={0.1} readOnly />
-                            <p className="ml-1 pt-[1px]">{product.rating.count}</p>
-                        </div>
+                <Loading />
+            ) : 
+            <div className="grid grid-cols-[1fr_0.5fr_0.5fr] grid-rows-[20px_350px_100px] mx-20 mt-[210px] max-w-[830px] mx-auto">                
+                <img className="col-span-1 object-contain row-span-4 h-[400px] justify-self-center pt-3" src={product.image} alt={product.title} />
+                <div className="row-span-1 col-span-2 place-self-center pb-5">
+                    <h1 className="mx-6 pt-10 pb-2 font-extrabold xs:text-[12px]">{product.title}</h1>
+                    <p className=" mx-6 dynamic-text">{product.description}</p>
+                    <div className="flex col-span-2 justify-center pt-4 mr-2">
+                        <Rating name='half-rating-read' defaultValue={product.rating.rate} precision={0.1} readOnly />
+                        <p className="ml-1 pt-[1px]">{product.rating.count}</p>
                     </div>
-                    <p className="font-extrabold justify-self-end mt-[15px]">${formatPrice(product)}</p>
-                    <div listid={product.id} className="text-center">
-                        <div className="inline-flex items-center border-black border-solid mx-auto border-2 rounded-lg mt-3 mr-4">
-                            <button onClick={incrementDown} className="font-bold pl-2 ">-</button>
-                            <input id="inputAmount" type="numerical" className="w-[25px] text-center justify-center p-0" 
-                            min="1"
-                            defaultValue="1"
-                            />
-                            <button onClick={incrementUp} className="font-bold pr-1 ">+</button>
-                        </div>
-                        <button onClick={handleAddMultiple} className="h-[36px] py-1 border border-black p-2 rounded-xl  bg-navy text-white hover:bg-white hover:text-navy">Add to Cart</button>  
+                </div>
+                <p className="font-extrabold justify-self-end mt-[15px]">${formatPrice(product)}</p>
+                <div listid={product.id} className="text-center">
+                    <div className="inline-flex items-center border-black border-solid mx-auto border-2 rounded-lg mt-3 mr-4">
+                        <button onClick={incrementDown} className="font-bold pl-2 ">-</button>
+                        <input id="inputAmount" type="numerical" className="w-[25px] text-center justify-center p-0" 
+                        min="1"
+                        defaultValue="1"
+                        />
+                        <button onClick={incrementUp} className="font-bold pr-1 ">+</button>
                     </div>
-                </>
+                    <button onClick={handleAddMultiple} className="h-[36px] py-1 border border-black p-2 rounded-xl  bg-navy text-white hover:bg-white hover:text-navy">Add to Cart</button>  
+                </div>
+            </div>
             }
         </div>
     )
 }
 
-export default SingleItem;
+export default SingleItem
